2d51ed44eca3bf0617bab346b7d2c0ce1e435cb4
Suman-Dhillon <119367545+Suman-Dhillon@users.noreply.github.com>
2023-10-07 16:59:14 -0600

    Week 3 Assignment
    Shopping List

--- app/page.js
+++ app/page.js
@@ -7,6 +7,8 @@ export default function Page(){
   <h1>CPRG 306: Web Development 2 - Assignments</h1>
   <StudentInfo />
   <Link href="/week2">Week-2</Link>
+  <br />
+  <Link href="/week3">Week-3</Link>
   </>
   );
 }
