5d88a2959dcc6347e40e9de7ab841633e94245d5
Suman-Dhillon <119367545+Suman-Dhillon@users.noreply.github.com>
2023-12-10 20:32:36 -0700

    Week 7
    Fetching data

--- app/page.js
+++ app/page.js
@@ -1,20 +1,24 @@
-import Link from 'next/link';
-import StudentInfo from './StudentInfo';
+import Link from "next/link";
+import StudentInfo from "./StudentInfo";
 
-export default function Page(){
-  return(
-  <>
-  <h1>CPRG 306: Web Development 2 - Assignments</h1>
-  <StudentInfo />
-  <Link href="/week2">Week-2</Link>
-  <br />
-  <Link href="/week3">Week-3</Link>
-  <br />
-  <Link href="/week4">Week-4</Link>
-  <br />
-  <Link href="/week5">Week-5</Link>
-  <br />
-  <Link href="/week6">Week-6</Link>
-  </>
+export default function Page() {
+  return (
+    <>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <StudentInfo />
+      <Link href="/week2">Week-2</Link>
+      <br />
+      <Link href="/week3">Week-3</Link>
+      <br />
+      <Link href="/week4">Week-4</Link>
+      <br />
+      <Link href="/week5">Week-5</Link>
+      <br />
+      <Link href="/week6">Week-6</Link>
+      <br />
+      <Link href="/week7">Week-7</Link>
+      <br />
+      <Link href="/week8">Week-8</Link>
+    </>
   );
 }
